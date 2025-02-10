08d9797c9853e57f27b070152faa5b419dd360d8
Maria Rosa <mariarosa@MacBook-Pro-de-Maria.local>
2025-02-10 16:45:31 -0300

    method to convert also feet added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static double convertToCentimeters(int heightInInches) {
+
+        return heightInInches * 2.54;
+    }
+
+    public  static double convertToCentimeters(int heightInFeet, int remainingHeightInInches) {
+        int heightInFeetConverted = heightInFeet * 12;
+        int totalHeight = heightInFeetConverted + remainingHeightInInches;
+        return convertToCentimeters(totalHeight);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter height in feet (a round value): ");
+
+        int heightInFeet = scanner.nextInt();
+
+        System.out.println("Now enter the remaining value of height in inches: ");
+
+        int remainingHeightInInches = scanner.nextInt();
+        double convertedHeight = convertToCentimeters(heightInFeet, remainingHeightInInches);
+
+        System.out.println("Converted height: " + convertedHeight);
+
+
+
+
+
     }
 }
